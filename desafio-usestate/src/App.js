bbcc76a9410c43c4c24dcd76416ed2368995c1aa
José Mariotto <josemarioto@gmail.com>
2023-07-31 22:03:34 -0300

    create Produto

---
+++ desafio-usestate/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Produto from './Produto';
+
+function App() {
+  const [dados, setDados] = React.useState(null);
+
+    function handleClick(event) {
+        console.log(event.target.innerText);
+    }
+
+  return (
+    <div >
+     <button style={{margin: '.5rem '}} onClick={handleClick}>notebook</button>
+     <button style={{margin: '.5rem '}} onClick={handleClick}>smartphone</button>
+     <button style={{margin: '.5rem '}} onClick={handleClick}>tablet</button>
+     <Produto />
+    </div>
+  );
+}
+
+export default App;
---
+++ desafio-usestate/src/Produto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Produto = () => {
+    
+  return (
+    <div></div>
+  )
+}
+
+export default Produto

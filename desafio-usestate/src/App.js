03e160127cf6c0a848db572226032c84a077104a
José Mariotto <josemarioto@gmail.com>
2023-08-01 20:06:32 -0300

    api

--- desafio-usestate/src/App.js
+++ desafio-usestate/src/App.js
@@ -3,9 +3,17 @@ import Produto from './Produto';
 
 function App() {
   const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
 
-    function handleClick(event) {
-        console.log(event.target.innerText);
+  async function handleClick(event) {
+        setCarregando(true);
+        const response = await fetch(
+          `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+          );
+        const json = await response.json();
+        setDados(json)
+        setCarregando(false);
+        ;
     }
 
   return (
@@ -13,7 +21,8 @@ function App() {
      <button style={{margin: '.5rem '}} onClick={handleClick}>notebook</button>
      <button style={{margin: '.5rem '}} onClick={handleClick}>smartphone</button>
      <button style={{margin: '.5rem '}} onClick={handleClick}>tablet</button>
-     <Produto />
+     {carregando && <p>Carregando...</p>}
+     {!carregando && dados && <Produto dados={dados}/>}
     </div>
   );
 }
--- desafio-usestate/src/Produto.js
+++ desafio-usestate/src/Produto.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Produto = () => {
+const Produto = ({dados}) => {
     
   return (
-    <div></div>
+    <div>
+      <h1>{dados.nome}</h1>
+      <p>R$ {dados.preco}</p>
+      <p>{dados.descricao}</p>
+      <img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
+    </div>
   )
 }
 
